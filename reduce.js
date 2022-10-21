59a3a615236ad8b09c360007c70c4ed0ec831ab2
eddieaviles357 <eduardoaviles357@yahoo.com>
2022-10-20 20:25:04 -0700

    optimized function

--- reduce.js
+++ reduce.js
@@ -1,67 +1,43 @@
-/*
-Write a function called extractValue which accepts an array of objects and a key and returns a new array with the value of each object at the key.
-
-Examples:
-    const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}]
-    extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
-*/
-
+// reduce
 function extractValue(arr, key) {
-    let extractedValues = [];
-    arr.reduce((accum, next) => {
-        if(extractedValues.length < 1) {
-            extractedValues.push(accum[key])
-        }
-        extractedValues.push(next[key])
-    })
-    return extractedValues;
+    return arr.reduce((accum, next) => {
+        accum.push(next[key]);
+        return accum;
+    }, [])
 }
 
 function vowelCount(str) {
     let vowels = {a:0,e:0,i:0,o:0,u:0};
-    Array.from(str.toLowerCase()).reduce((_, nextVal) => {
-        if(vowels.hasOwnProperty(nextVal)) vowels[nextVal]++;
-    }, str[0])
-    return vowels
+    let arr = Array.from(str.toLowerCase());
+    return arr.reduce((accum, next) => {
+        if(vowels.hasOwnProperty(next)) {
+            vowels[next]++
+            return {...accum, [next] : vowels[next]}
+        }
+        return {...accum};
+    }, {});
 }
 
-/*
-Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
+function addKeyAndValue(arr, key, value) {
+    arr.reduce((acc, next,i) => {
+        next[key] = value
+    }, {});
+}
 
-Examples:
-    const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}];
-    
-    addKeyAndValue(arr, 'title', 'Instructor') // 
-      [
-        {title: 'Instructor', name: 'Elie'}, 
-        {title: 'Instructor', name: 'Tim'}, 
-        {title: 'Instructor', name: 'Matt'}, 
-        {title: 'Instructor', name: 'Colt'}
-       ]
-*/
-
-function addKeyAndValue(arr, key, value) {}
-
-/*
-Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
-
-Examples:
-    
-    function isEven(val){
-        return val % 2 === 0;
-    }
-    
-    const arr = [1,2,3,4,5,6,7,8];
-    
-    partition(arr, isEven) // [[2,4,6,8], [1,3,5,7]];
-    
-    function isLongerThanThreeCharacters(val){
-        return val.length > 3;
-    }
-    
-    const names = ['Elie', 'Colt', 'Tim', 'Matt'];
-    
-    partition(names, isLongerThanThreeCharacters) // [['Elie', 'Colt', 'Matt'], ['Tim']]
-*/
-
-function partition(arr, callback) {}
+function isLongerThanThreeCharacters(val){
+    return val.length > 3;
+}
+function isEven(val){
+    return val % 2 === 0;
+}
+var arr = [1,2,3,4,5,6,7,8];
+function partition(arr, callback) {
+    return arr.reduce((acc, next) => {
+            if(callback(next)) {
+                acc[0].push(next);
+            } else {
+                acc[1].push(next);
+            }
+    }, [[],[]])
+}
+partition(arr, isEven);
